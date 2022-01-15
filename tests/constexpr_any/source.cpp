d2ac916eda70cecb72d01003cee5b0f84101fe5a
Michael Rizkalla <Michael.Anis1995@gmail.com>
2022-01-15 14:24:05 +0000

    Add runtime tests

--- tests/constexpr_any/source.cpp
+++ tests/constexpr_any/source.cpp
@@ -6,9 +6,9 @@
 #include <string>
 #include <vector>
 
-#define ASSERT(x)     \
-    if (!(x)) {       \
-        return false; \
+#define CONSTEXPR_ASSERT(x) \
+    if (!(x)) {             \
+        return false;       \
     }
 
 namespace test_helpers {
@@ -93,7 +93,7 @@ namespace test_helpers {
         auto val = mr::any_cast< T >(&Any);
         if (val) {
             bool comp = val->GetValue() == u;
-            ASSERT(comp);
+            CONSTEXPR_ASSERT(comp);
             return true;
         }
         return false;
@@ -115,7 +115,7 @@ namespace test {
 
         TestType t {};
         std::construct_at(&t.a, T { 5 });
-        ASSERT(test_helpers::CheckValue< T >(t.a, 5))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(t.a, 5))
 
 #ifndef _MSC_VER
         std::destroy_at(&t.a);
@@ -134,10 +134,13 @@ namespace test {
 
             t1 = t2; // const copy
 
-            ASSERT(test_helpers::HasType< Type2 >(t1))
-            ASSERT(test_helpers::HasType< Type2 >(t2))
-            ASSERT(test_helpers::CheckValue< Type2 >(t1, 2))
-            ASSERT(test_helpers::CheckValue< Type2 >(t2, 2))
+            if constexpr (!std::same_as< Type1, Type2 >) {
+                CONSTEXPR_ASSERT(!test_helpers::HasType< Type1 >(t1))
+            }
+            CONSTEXPR_ASSERT(test_helpers::HasType< Type2 >(t1))
+            CONSTEXPR_ASSERT(test_helpers::HasType< Type2 >(t2))
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type2 >(t1, 2))
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type2 >(t2, 2))
         }
 
         // Copy from and to empty any
@@ -147,10 +150,10 @@ namespace test {
 
             t1 = t2;
 
-            ASSERT(test_helpers::HasType< Type1 >(t1));
-            ASSERT(test_helpers::HasType< Type1 >(t2));
-            ASSERT(test_helpers::CheckValue< Type1 >(t1, 5));
-            ASSERT(test_helpers::CheckValue< Type1 >(t2, 5));
+            CONSTEXPR_ASSERT(test_helpers::HasType< Type1 >(t1));
+            CONSTEXPR_ASSERT(test_helpers::HasType< Type1 >(t2));
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type1 >(t1, 5));
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type1 >(t2, 5));
         }
         {
             mr::any       t1 { Type2 { 6 } };
@@ -158,15 +161,15 @@ namespace test {
 
             t1 = t2;
 
-            ASSERT(!test_helpers::HasType< Type2 >(t1));
-            ASSERT(!test_helpers::HasType< Type2 >(t2));
+            CONSTEXPR_ASSERT(!test_helpers::HasType< Type2 >(t1));
+            CONSTEXPR_ASSERT(!test_helpers::HasType< Type2 >(t2));
         }
 
         // Assign to myself is no-op
         {
             mr::any t {};
             t = static_cast< mr::any& >(t);
-            ASSERT(!t.has_value());
+            CONSTEXPR_ASSERT(!t.has_value());
         }
         {
             mr::any t { Type1 { 1 } };
@@ -196,9 +199,12 @@ namespace test {
 
             a1 = std::move(a2);
 
-            ASSERT(a1.has_value());
-            ASSERT(!a2.has_value());
-            ASSERT(test_helpers::CheckValue< Type2 >(a1, 3));
+            if constexpr (!std::same_as< Type1, Type2 >) {
+                CONSTEXPR_ASSERT(!test_helpers::HasType< Type1 >(a1))
+            }
+            CONSTEXPR_ASSERT(a1.has_value());
+            CONSTEXPR_ASSERT(!a2.has_value());
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type2 >(a1, 3));
         }
 
         // Move assign from and to empty any
@@ -207,12 +213,12 @@ namespace test {
             mr::any t1;
             mr::any t2 { Type1 { 1 } };
 
-            ASSERT(!t1.has_value());
+            CONSTEXPR_ASSERT(!t1.has_value());
             t1 = std::move(t2);
 
-            ASSERT(t1.has_value());
-            ASSERT(!t2.has_value());
-            ASSERT(test_helpers::CheckValue< Type1 >(t1, 1));
+            CONSTEXPR_ASSERT(t1.has_value());
+            CONSTEXPR_ASSERT(!t2.has_value());
+            CONSTEXPR_ASSERT(test_helpers::CheckValue< Type1 >(t1, 1));
         }
         {
             mr::any t1 { Type2 { 2 } };
@@ -220,8 +226,8 @@ namespace test {
 
             t1 = std::move(t2);
 
-            ASSERT(!test_helpers::HasType< Type2 >(t1));
-            ASSERT(!t1.has_value());
+            CONSTEXPR_ASSERT(!test_helpers::HasType< Type2 >(t1));
+            CONSTEXPR_ASSERT(!t1.has_value());
         }
 
         return true;
@@ -248,11 +254,11 @@ namespace test {
         a4.emplace< T >(test_helpers::UseTemplateCtor {}, 5);
         a5.emplace< T >(test_helpers::UseTemplateCtor {}, test_helpers::ConvertibleToInt { 6 });
 
-        ASSERT(test_helpers::CheckValue< T >(a1, 1))
-        ASSERT(test_helpers::CheckValue< T >(a2, 2))
-        ASSERT(test_helpers::CheckValue< T >(a3, 4))
-        ASSERT(test_helpers::CheckValue< T >(a4, 5))
-        ASSERT(test_helpers::CheckValue< T >(a5, 6))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(a1, 1))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(a2, 2))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(a3, 4))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(a4, 5))
+        CONSTEXPR_ASSERT(test_helpers::CheckValue< T >(a5, 6))
 
         return true;
     }
@@ -262,28 +268,28 @@ namespace test {
         const mr::any a1 = T {};
 
         auto valuePtr = mr::any_cast< T >(&a1);
-        ASSERT(*valuePtr == T {})
-        ASSERT(test_helpers::HasType< T >(a1))
-        ASSERT(a1.has_value())
+        CONSTEXPR_ASSERT(*valuePtr == T {})
+        CONSTEXPR_ASSERT(test_helpers::HasType< T >(a1))
+        CONSTEXPR_ASSERT(a1.has_value())
 
         if constexpr (std::is_fundamental_v< T >) {
             mr::any a2     = static_cast< T >(5);
             auto    value2 = mr::any_cast< T >(a2);
-            ASSERT(value2 == static_cast< T >(5))
-            ASSERT(a2.has_value())
+            CONSTEXPR_ASSERT(value2 == static_cast< T >(5))
+            CONSTEXPR_ASSERT(a2.has_value())
 
             mr::any a3     = static_cast< T >(10);
             auto    value3 = mr::any_cast< T&& >(std::move(a3));
-            ASSERT(value3 == static_cast< T >(10))
+            CONSTEXPR_ASSERT(value3 == static_cast< T >(10))
         } else {
             mr::any a2     = T { 5 };
             auto    value2 = mr::any_cast< T >(a2);
-            ASSERT(value2 == T { 5 })
-            ASSERT(a2.has_value())
+            CONSTEXPR_ASSERT(value2 == T { 5 })
+            CONSTEXPR_ASSERT(a2.has_value())
 
             mr::any a3     = T { 10 };
             auto    value3 = mr::any_cast< T >(std::move(a3));
-            ASSERT(value3 == T { 10 })
+            CONSTEXPR_ASSERT(value3 == T { 10 })
         }
 
         return true;
@@ -378,4 +384,59 @@ static_assert(test::Strlen< std::string >(std::string {
 #endif // __cpp_lib_constexpr_string
 
 int main() {
+
+    auto result = test::TestCopy< test_helpers::Object< test_helpers::SmallSizeObject >,
+                                  test_helpers::Object< test_helpers::SmallSizeObject > >();
+    assert(result);
+    result = test::TestCopy< test_helpers::Object< test_helpers::LargeSizeObject >,
+                             test_helpers::Object< test_helpers::LargeSizeObject > >();
+    assert(result);
+    result = test::TestCopy< test_helpers::Object< test_helpers::SmallSizeObject >,
+                             test_helpers::Object< test_helpers::LargeSizeObject > >();
+    assert(result);
+    result = test::TestCopy< test_helpers::Object< test_helpers::LargeSizeObject >,
+                             test_helpers::Object< test_helpers::SmallSizeObject > >();
+    assert(result);
+
+    result = test::TestMove< test_helpers::Object< test_helpers::SmallSizeObject >,
+                             test_helpers::Object< test_helpers::SmallSizeObject > >();
+    assert(result);
+    result = test::TestMove< test_helpers::Object< test_helpers::LargeSizeObject >,
+                             test_helpers::Object< test_helpers::LargeSizeObject > >();
+    assert(result);
+    result = test::TestMove< test_helpers::Object< test_helpers::SmallSizeObject >,
+                             test_helpers::Object< test_helpers::LargeSizeObject > >();
+    assert(result);
+    result = test::TestMove< test_helpers::Object< test_helpers::LargeSizeObject >,
+                             test_helpers::Object< test_helpers::SmallSizeObject > >();
+    assert(result);
+
+    assert(test::MoveAssignNoExcept());
+
+    assert(test::EmplaceTests< test_helpers::Object< test_helpers::SmallSizeObject > >());
+    assert(test::EmplaceTests< test_helpers::Object< test_helpers::LargeSizeObject > >());
+
+    assert(test::CastTests< int >());
+    assert(test::CastTests< float >());
+#ifdef __cpp_lib_constexpr_string
+    assert(test::CastTests< std::string >());
+#endif // __cpp_lib_constexpr_string
+#ifdef __cpp_lib_constexpr_vector
+    assert(test::CastTests< std::vector< int > >());
+#endif // __cpp_lib_constexpr_vector
+
+    assert(test::GetMaxValues< int >({ 1, 2, 3, 4, 9, 5, 6 }) == 9);
+    assert(test::GetMaxValues< std::uint64_t >({ std::uint64_t { 10 }, std::uint64_t { 2 }, std::uint64_t { 31 },
+                                                 std::uint64_t { 44 }, std::uint64_t { 19 }, std::uint64_t { 75 },
+                                                 std::uint64_t { 46 } }) == std::uint64_t { 75 });
+
+    assert(test::Strlen< const char* >("this is const char* string") == 26);
+    using namespace std::literals::string_view_literals;
+    assert(test::Strlen< std::string_view >("this is std::string_view string"sv) == 31);
+
+#ifdef __cpp_lib_constexpr_string
+    assert(test::Strlen< std::string >(std::string { "small string" }) == 12);
+    assert(test::Strlen< std::string >(
+               std::string { "A large string that will not fit into a small string optimization!" }) == 66);
+#endif // __cpp_lib_constexpr_string
 }
